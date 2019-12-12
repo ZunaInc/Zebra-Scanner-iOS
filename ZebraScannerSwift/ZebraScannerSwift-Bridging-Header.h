f6cd19ef187e11cc78d217af29214e12499be8aa
VijayAtla <vijay.atla@goalsr.com>
2019-12-12 12:15:06 +0530

    Zebra Scanner Multi OS SDK integrated to Swift 3+

---
+++ ZebraScannerSwift/ZebraScannerSwift-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  ZebraScannerSwift-Bridging-Header.h
+//  ZebraScannerSwift
+//
+//  Created by Vijay A on 29/11/19.
+//  Copyright © 2019 Goalsr. All rights reserved.
+//
+
+#ifndef ZebraScannerSwift_Bridging_Header_h
+#define ZebraScannerSwift_Bridging_Header_h
+
+#import "SbtSdkFactory.h"
+#import "ConnectionManager.h"
+#import "ScannerAppEngine.h"
+
+#endif /* ZebraScannerSwift_Bridging_Header_h */
